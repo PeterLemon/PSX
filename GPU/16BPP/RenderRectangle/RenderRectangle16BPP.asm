000d4a8cb8949d0065de6aeb4bfe465be8fe4858
PeterLemon <krom@gbadev.org>
2017-02-28 18:33:51 +0000

    GPU Render Rectangle Demo

--- GPU/16BPP/RenderRectangle/RenderRectangle16BPP.asm
+++ GPU/16BPP/RenderRectangle/RenderRectangle16BPP.asm
@@ -14,7 +14,7 @@ WRGP1 GPURESET,0  ; Write GP1 Command Word (Reset GPU)
 WRGP1 GPUDISPEN,0 ; Write GP1 Command Word (Enable Display)
 WRGP1 GPUDISPM,HRES320+VRES240+BPP15+VNTSC ; Write GP1 Command Word (Set Display Mode: 320x240, 15BPP, NTSC)
 WRGP1 GPUDISPH,0xC60260 ; Write GP1 Command Word (Horizontal Display Range 608..3168)
-WRGP1 GPUDISPV,0x041C17 ; Write GP1 Command Word (Vertical Display Range 23..263)
+WRGP1 GPUDISPV,0x042018 ; Write GP1 Command Word (Vertical Display Range 24..264)
 
 ; Setup Drawing Area
 WRGP0 GPUDRAWM,0x000400   ; Write GP0 Command Word (Drawing To Display Area Allowed Bit 10)
