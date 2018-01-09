b26c0b6f919784f70802dd95519e41b4af5ec879
PeterLemon <krom@gbadev.org>
2018-01-09 15:31:07 +0000

    Fix Demos To Compile On Linux Thanks edgbla

--- GPU/16BPP/MemoryTransfer/MemoryTransfer16BPP.asm
+++ GPU/16BPP/MemoryTransfer/MemoryTransfer16BPP.asm
@@ -1,6 +1,6 @@
 ; PSX 'Bare Metal' GPU 16BPP Memory Transfer Demo by krom (Peter Lemon):
 .psx
-.create "MemoryTransfer16BPP.bin", 0
+.create "MemoryTransfer16BPP.bin", 0x80010000
 
 .include "LIB/PSX.INC" ; Include PSX Definitions
 .include "LIB/PSX_GPU.INC" ; Include PSX GPU Definitions & Macros
