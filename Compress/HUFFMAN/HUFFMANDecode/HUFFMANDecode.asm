32cb52b7869a93b2e3229d3b639691d6cd96826e
peterlemon <krom@gbadev.org>
2020-04-26 07:06:15 +0100

    Optimize Huffman Compression Demo

--- Compress/HUFFMAN/HUFFMANDecode/HUFFMANDecode.asm
+++ Compress/HUFFMAN/HUFFMANDecode/HUFFMANDecode.asm
@@ -63,16 +63,13 @@ HuffChunkLoop:
       andi t7,-2   ; T7 = Tree Offset NOT 1
       addu t7,t5   ; T7 = Node0 Child Offset
       and t5,t2,t3 ; Test Node Bit (0 = Node0, 1 = Node1)
-      beqz t5,HuffNode0
-      srl t3,1     ; Shift T3 To Next Node Bit (Delay Slot)
-      addiu t7,1   ; T7 = Node1 Child Offset
-      j HuffNodeEnd
-      andi t4,0x40   ; T4 = Test Node1 End Flag (Delay Slot)
-      HuffNode0:
-        andi t4,0x80 ; T4 = Test Node0 End Flag
+      beqz t5,HuffNodeEnd
+      andi t5,t4,0x80 ; T5 = Test Node0 End Flag (Delay Slot)
+      andi t5,t4,0x40 ; T5 = Test Node1 End Flag
+      addiu t7,1      ; T7 = Node1 Child Offset
       HuffNodeEnd:
-        beqz t4,HuffByteLoop ; Test Node End Flag (1 = Next Child Node Is Data)
-        nop ; Delay Slot
+        beqz t5,HuffByteLoop ; Test Node End Flag (1 = Next Child Node Is Data)
+        srl t3,1 ; Shift T3 To Next Node Bit (Delay Slot)
         j HuffByteLoop
         ori t6,r0,1 ; T6 = Leaf (Delay Slot)
   HuffEnd:
